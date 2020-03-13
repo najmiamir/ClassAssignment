852f65b700a5534e66fc695dda6eb933a4539a41
najmiamir <61965073+najmiamir@users.noreply.github.com>
2020-03-13 16:33:38 +0800

    Create main.mjs

---
+++ main.mjs
@@ -0,0 +1,21 @@
+
+import readline from 'readline';
+import {Student} from './exercise2.mjs';
+
+const rl = readline.createInterface({
+
+input: process.stdin,
+
+output: process.stdout
+});
+
+rl.question('What is your name? : ', (answer1) => {
+    rl.question('what is yout matricno : ', (answer2) => {
+        rl.question('What is your major : ', (answer3) => {
+
+            let student = new Student (answer1, answer2, answer3)
+            console.log(student.display());
+            rl.close();
+        });
+    });
+});
